490be1d028fdc5452cbd31f9b98bb473c7636ac2
santosjn <jeansjcsp@gmail.com>
2018-05-06 22:20:55 -0300

    handling errors, solving cross origin

--- app.js
+++ app.js
@@ -2,17 +2,24 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require("./api/routes/products");
 const ordersRoutes = require("./api/routes/orders");
 
-// app.use((req, res, next) => {
-//     res.status(200).json({
-//         message: 'It Works!!!'
-//     });
-// });
-
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 // Routes description
 app.use('/products', productRoutes);
